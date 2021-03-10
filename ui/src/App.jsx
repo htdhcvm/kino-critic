20351572a009adec581564b6e19957db311450a7
flouheforst <flouheforst@gmail.com>
2021-03-10 16:52:20 +0300

    init ui

---
+++ ui/src/App.jsx
@@ -0,0 +1,11 @@
+import React, { useEffect } from 'react';
+
+const App = () => {
+    return (
+        <div className="App">
+            <h1>Kino critic</h1>
+        </div>
+    );
+};
+
+export default App;
---
+++ ui/webpack.config.js
@@ -0,0 +1,34 @@
+const path = require('path');
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src/index.jsx'),
+    devtool: 'inline-source-map',
+    mode: 'development',
+    output: {
+        path: path.resolve(__dirname, 'dist/'),
+        publicPath: path.resolve(__dirname, 'dist/'),
+        filename: 'bundle.js',
+    },
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader'],
+            },
+        ],
+    },
+    devServer: {
+        contentBase: path.join(__dirname, 'public/'),
+        port: 3001,
+        publicPath: 'http://localhost:3001/dist/',
+        hot: true,
+    },
+};
