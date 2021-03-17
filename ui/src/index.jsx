40e93cc38a03345659bd2edfdcb844aac7fb3995
flouheforst <flouheforst@gmail.com>
2021-03-17 11:20:26 +0300

    provider store in application

--- ui/src/index.jsx
+++ ui/src/index.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
 
 import App from './App';
 
-ReactDOM.render(<App></App>, document.getElementById('root'));
+import store from './store';
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root'),
+);
