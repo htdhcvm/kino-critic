114d17641b231938de37cf4975d0253fa9ed4a88
flouheforst <flouheforst@gmail.com>
2021-03-31 23:02:38 +0300

    initial assembly entyty with visitor grant

---
+++ api-server/model/bootstrap/bootstrapVisitor.js
@@ -0,0 +1,30 @@
+const { sequelizeVisitor } = require('../');
+
+const FilmsModel = require('../Entity/kinos/Kinos');
+const CommentsModel = require('../Entity/comments/Comments');
+const UserModel = require('../Entity/users_kino/User');
+
+module.exports = () => {
+    const filmsModelWithVisitorRole = FilmsModel(sequelizeVisitor);
+    const userModelWithVisitorRole = UserModel(sequelizeVisitor);
+    const commentsModelWithVisitorRole = CommentsModel(sequelizeVisitor);
+
+    userModelWithVisitorRole.hasMany(filmsModelWithVisitorRole, {
+        as: 'userHasManyFilms',
+    });
+    filmsModelWithVisitorRole.belongsTo(userModelWithVisitorRole);
+
+    userModelWithVisitorRole.belongsToMany(filmsModelWithVisitorRole, {
+        as: 'usersHasManyFilmsWithComments',
+        through: 'comments',
+    });
+    filmsModelWithVisitorRole.belongsToMany(userModelWithVisitorRole, {
+        through: 'comments',
+    });
+
+    return {
+        filmsModelWithVisitorRole,
+        userModelWithVisitorRole,
+        commentsModelWithVisitorRole,
+    };
+};
