d60b5f32160d472a6693ba3b979be865c72e3b90
flouheforst <flouheforst@gmail.com>
2021-03-31 21:46:01 +0300

    init controller admin

--- api-server/controllers/admin.controller.js
+++ api-server/controllers/admin.controller.js
@@ -1,3 +1,43 @@
-class Admin {}
+const FromPrimitivesToManagerDTO = require('../model/DTO/FromPrimitive/FromPrimitivesToManagerDTO');
 
-module.exports = Admin;
+class AdminController {
+    constructor(adminService) {
+        this.adminService = adminService;
+    }
+    async addNewManager(req, res) {
+        try {
+            const {
+                login,
+                password,
+                age,
+                fio,
+                address,
+                typeUser,
+                number,
+                format,
+                numberFormat,
+            } = req.body;
+
+            const resultAddNewManager = this.adminService.addNewManager(
+                FromPrimitivesToManagerDTO({
+                    login,
+                    password,
+                    age,
+                    fio,
+                    address,
+                    typeUser,
+                    number,
+                    format,
+                    numberFormat,
+                }),
+            );
+
+            res.sendStatus(200);
+        } catch (error) {
+            console.log(error);
+            return res.sendStatus(500);
+        }
+    }
+}
+
+module.exports = AdminController;
