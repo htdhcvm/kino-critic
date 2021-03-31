ea07a7a87a70f7f6e21c634a373034e495ccaf6a
flouheforst <flouheforst@gmail.com>
2021-03-31 23:03:37 +0300

    flash is cool

--- api-server/server.js
+++ api-server/server.js
@@ -2,5 +2,5 @@ const app = require('./app');
 require('./config')(app);
 
 app.listen(process.env.PORT, () => {
-    console.log(`Server has been started on ${process.env.PORT} port`);
+    console.log(`⚡️ Server has been started on ${process.env.PORT} port`);
 });
