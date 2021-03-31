eb32531a97d8603067469d45eaaf5610f3e719c6
flouheforst <flouheforst@gmail.com>
2021-03-31 23:08:24 +0300

    several connections to database, for several types of users (manager, admin, visitor, user)

--- api-server/model/index.js
+++ api-server/model/index.js
@@ -1,19 +1,53 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
+const base = {
     dialect: process.env.DB_DIALECT,
     database: process.env.DB_NAME,
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    logging: false,
+};
+
+const sequelizeVisitor = new Sequelize({
+    ...base,
+    username: process.env.DB_USERNAME_VISITOR,
+    password: process.env.DB_PASSWORD_VISITOR,
 });
 
-(async () => {
-    try {
-        await sequelize.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
-    }
-})();
+const sequelizeUser = new Sequelize({
+    ...base,
+    username: process.env.DB_USERNAME_USER,
+    password: process.env.DB_PASSWORD_USER,
+});
 
-global.sequelize = sequelize;
+const sequelizeManager = new Sequelize({
+    ...base,
+    username: process.env.DB_USERNAME_MANAGER,
+    password: process.env.DB_PASSWORD_MANAGER,
+});
+
+const sequelizeAdmin = new Sequelize({
+    ...base,
+    username: process.env.DB_USERNAME_ADMIN,
+    password: process.env.DB_PASSWORD_ADMIN,
+});
+
+Promise.all([
+    sequelizeUser.authenticate(),
+    sequelizeManager.authenticate(),
+    sequelizeAdmin.authenticate(),
+    sequelizeVisitor.authenticate(),
+])
+    .then(_ => {
+        console.log(
+            'All connection was success completed with roles - [Visitor, User, Manager, Admin]',
+        );
+    })
+    .catch(error => {
+        console.error('Connections falid ', error);
+    });
+
+module.exports = {
+    sequelizeVisitor,
+    sequelizeUser,
+    sequelizeManager,
+    sequelizeAdmin,
+};
