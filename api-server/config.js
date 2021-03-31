4831988526e2c522a7c044c0912aef1c5e7bf680
flouheforst <flouheforst@gmail.com>
2021-03-31 21:30:04 +0300

    init and setting base packages. init base routing

--- api-server/config.js
+++ api-server/config.js
@@ -1,12 +1,18 @@
 require('dotenv').config();
 require('./model');
 
+const bodyParser = require('body-parser');
+
 const adminRouter = require('./routes/admin.routes');
 const userRouter = require('./routes/user.routes');
 const managerRouter = require('./routes/manager.routes');
+const visitorRouter = require('./routes/visitor.routes');
 
 module.exports = app => {
+    app.use(bodyParser.json());
+
     app.use('/api', adminRouter);
     app.use('/api', userRouter);
     app.use('/api', managerRouter);
+    app.use('/api', visitorRouter);
 };
